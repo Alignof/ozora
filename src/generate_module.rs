a089cea5d225a13b72574dedc133aaa65aa0c487
Alingof <Alignof@outlook.com>
2024-12-10 22:33:29 +0900

    [update] Thank you, dtolnay

--- src/generate_module.rs
+++ src/generate_module.rs
@@ -4,16 +4,6 @@ use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
-/// Importing module statements.
-const IMPORT_MODULE: &str = r#"
-use super::{{pseudo_vs_exception, EmulateExtension, EmulatedCsr}};
-use crate::HYPERVISOR_DATA;
-
-use core::cell::OnceCell;
-use spin::Mutex;
-//use raki::{{Instruction, OpcodeKind, ZicfissOpcode, ZicsrOpcode}};
-"#;
-
 /// Genarate hypervisor module.
 pub fn create_hikami_module(
     ext_name: String,
@@ -21,64 +11,78 @@ pub fn create_hikami_module(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::create(output_path)?;
     writeln!(file, "//! Emulation {ext_name}")?;
-    writeln!(file, "{IMPORT_MODULE}")?;
+    indoc::writedoc!(
+        file,
+        "
+        use super::{{pseudo_vs_exception, EmulateExtension, EmulatedCsr}};
+        use crate::HYPERVISOR_DATA;
+
+        use core::cell::OnceCell;
+        use spin::Mutex;
+        use raki::{{Instruction, OpcodeKind, {ext_name}Opcode, ZicsrOpcode}};
+
+        "
+    )?;
 
     // generate a global data
-    writeln!(
+    indoc::writedoc!(
         file,
-        r#"
-/// Singleton for {ext_name}.
-pub static mut {}_DATA: Mutex<OnceCell<{ext_name}>> = Mutex::new(OnceCell::new());
-"#,
+        "
+        /// Singleton for {ext_name}.
+        pub static mut {}_DATA: Mutex<OnceCell<{ext_name}>> = Mutex::new(OnceCell::new());
+
+        ",
         ext_name.to_uppercase()
     )?;
 
     // generate struct definition.
-    writeln!(
+    indoc::writedoc!(
         file,
-        r#"
-/// Singleton for {ext_name} extension
-pub struct {ext_name};
+        "
+        /// Singleton for {ext_name} extension
+        pub struct {ext_name};
 
-impl {ext_name} {{
-    /// Constructor for `{ext_name}`.
-    pub fn new() -> Self {{
-        {ext_name}
-    }}
-}}
-"#,
+        impl {ext_name} {{
+            /// Constructor for `{ext_name}`.
+            pub fn new() -> Self {{
+                {ext_name}
+            }}
+        }}
+
+        "
     )?;
 
     // generate implementation of ExtensionEmulation trait.
-    writeln!(
+    indoc::writedoc!(
         file,
         r#"
-impl EmulateExtension for {ext_name} {{
-    /// Emulate {ext_name} instruction.
-    #[allow(clippy::cast_possible_truncation)]
-    fn instruction(&mut self, inst: &Instruction) {{
-        let mut context = unsafe {{ HYPERVISOR_DATA.lock() }}
-            .get()
-            .unwrap()
-            .guest()
-            .context;
+        impl EmulateExtension for {ext_name} {{
+            /// Emulate {ext_name} instruction.
+            #[allow(clippy::cast_possible_truncation)]
+            fn instruction(&mut self, inst: &Instruction) {{
+                let mut context = unsafe {{ HYPERVISOR_DATA.lock() }}
+                    .get()
+                    .unwrap()
+                    .guest()
+                    .context;
 
-        match inst.opc {{
-            _ => todo!("Implementing {ext_name} instruction emulation"),
+                match inst.opc {{
+                    _ => todo!("Implementing {ext_name} instruction emulation"),
+                }}
+            }}
+
+            /// Emulate Zicfiss CSRs access.
+            fn csr(&mut self, inst: &Instruction) {{
+                todo!("Implementing {ext_name} CSR emulation");
+            }}
+
+            /// Emulate CSR field that already exists.
+            fn csr_field(&mut self, inst: &Instruction, write_to_csr_value: u64, read_csr_value: &mut u64) {{
+                todo!("Implementing {ext_name} CSR field emulation");
+            }}
         }}
-    }}
-
-    /// Emulate Zicfiss CSRs access.
-    fn csr(&mut self, inst: &Instruction) {{
-        todo!("Implementing {ext_name} CSR emulation");
-    }}
-
-    /// Emulate CSR field that already exists.
-    fn csr_field(&mut self, inst: &Instruction, write_to_csr_value: u64, read_csr_value: &mut u64) {{
-        todo!("Implementing {ext_name} CSR field emulation");
-    }}
-}}
-"#,
+        "#,
     )?;
+
     Ok(())
 }
